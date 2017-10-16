4bed18335d6b9d23448f0be4577043414642e2ff
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-16 23:27:16 +0300

    add action

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 import program from 'commander';
+import genDiff from '../index';
 
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig>, <secondConfig>')
   .option('-f, --format [type]', 'Output format')
-  .parse(process.argv);
+  .action((firstConfig, secondConfig) => genDiff(firstConfig, secondConfig));
+
+program.parse(process.argv);
