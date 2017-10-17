73ceed6c5274c7bfb659c555d3eeb118695659a7
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-17 22:34:15 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
+import fs from 'fs';
+import ini from 'ini';
+import yaml from 'js-yaml';
 import _ from 'lodash';
-import Json from '../src/Json';
-import Yaml from '../src/Yaml';
-import Ini from '../src/Ini';
-import GenDiffAdapter from '../src/GenDiffAdapter';
+import compareParsedData from '../src/compareParsedData';
 
 export default (filePath1, filePath2) => {
-  const parserList = [
+  const parsersList = [
     {
-      name: Json,
+      parser: file => JSON.parse(file),
       check: arg => (arg.indexOf('json') + 1),
     },
     {
-      name: Yaml,
+      parser: file => yaml.safeLoad(file),
       check: arg => (arg.indexOf('yml') + 1),
     },
     {
-      name: Ini,
+      parser: file => ini.parse(file),
       check: arg => (arg.indexOf('ini') + 1),
     },
   ];
-  const getParser = arg => _.find(parserList, ({ check }) => check(arg));
-  const Parser = getParser(filePath1).name;
-  const genDiff = new Parser(filePath1, filePath2);
-  const genDiffAdapter = new GenDiffAdapter(genDiff);
-  return genDiffAdapter.compareFiles();
+  const dataOfFile1 = fs.readFileSync(filePath1, 'utf8');
+  const dataOfFile2 = fs.readFileSync(filePath2, 'utf8');
+  const getParser = arg => _.find(parsersList, ({ check }) => check(arg));
+  const { parser } = getParser(filePath1);
+  const parsedDataOfFile1 = parser(dataOfFile1);
+  const parsedDataOfFile2 = parser(dataOfFile2);
+  const result = compareParsedData(parsedDataOfFile1, parsedDataOfFile2);
+  return ['{', ...result, '}'].join('\n');
 };
