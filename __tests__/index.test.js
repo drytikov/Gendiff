80b901771d0b02749c7ca3b68adf4a7f082a6450
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-17 22:34:42 +0300

    style

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import genDiff from '../src/index';
+import genDiff from '../src';
 
 const result = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
 
