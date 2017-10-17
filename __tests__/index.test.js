e4e3db99f7871da7b7c845bb366c4d35dcaf2b91
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-17 13:54:38 +0300

    add test fo yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,10 @@ import genDiff from '../src/index';
 
 const result = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
 
-test('genDiff', () => {
+test('genDiff(JSON)', () => {
   expect(genDiff('__tests__/fixtures/before.json', '__tests__/fixtures/after.json')).toBe(result);
+});
+
+test('genDiff(YAML)', () => {
+  expect(genDiff('__tests__/fixtures/before.yml', '__tests__/fixtures/after.yml')).toBe(result);
 });
