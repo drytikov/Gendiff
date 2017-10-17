02cb42966a201cbdeed4b257f385ad9fec825beb
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-17 12:28:02 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import fs from 'fs';
+import _ from 'lodash';
 
 export default (filePath1, filePath2) => {
   const file1 = JSON.parse(fs.readFileSync(filePath1, 'utf8'));
   const file2 = JSON.parse(fs.readFileSync(filePath2, 'utf8'));
-  const newKeys = Object.keys(file2).reduce((acc, key) =>
-    (file1[key] ? acc : [...acc, `  + ${key}: ${file2[key]}`]), []);
-  const processedFile1 = Object.keys(file1).reduce((acc, key) => {
-    if (file2[key]) {
-      if (file2[key] === file1[key]) {
-        return [...acc, `    ${key}: ${file2[key]}`];
-      }
+  const uniqueItems = _.union(Object.keys(file1), Object.keys(file2));
+  const result = uniqueItems.reduce((acc, key) => {
+    if (file1[key] && !file2[key]) {
+      return [...acc, `  - ${key}: ${file1[key]}`];
+    }
+    if (file2[key] && !file1[key]) {
+      return [...acc, `  + ${key}: ${file2[key]}`];
+    }
+    if (file2[key] !== file1[key]) {
       return [...acc, `  + ${key}: ${file2[key]}`, `  - ${key}: ${file1[key]}`];
     }
-    return [...acc, `  - ${key}: ${file1[key]}`];
+    return [...acc, `    ${key}: ${file2[key]}`];
   }, []);
-  return ['{', ...processedFile1, ...newKeys, '}'].join('\n');
+  return ['{', ...result, '}'].join('\n');
 };
