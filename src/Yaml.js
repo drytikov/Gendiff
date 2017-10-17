1bb3b020c5804f947d8a1eb349623155ad5d774a
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-17 22:30:34 +0300

    delete

--- src/Yaml.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import yaml from 'js-yaml';
-
-export default class {
-  constructor(filePath1, filePath2) {
-    this.file1 = yaml.safeLoad(fs.readFileSync(filePath1, 'utf8'));
-    this.file2 = yaml.safeLoad(fs.readFileSync(filePath2, 'utf8'));
-  }
-}
