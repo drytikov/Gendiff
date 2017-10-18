11e5723b897a3100501a28fb2fba0c9dc099859e
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-18 15:19:04 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,30 +1,21 @@
 import fs from 'fs';
+import path from 'path';
 import ini from 'ini';
 import yaml from 'js-yaml';
-import _ from 'lodash';
 import compareParsedData from '../src/compareParsedData';
 
 export default (filePath1, filePath2) => {
-  const parsersList = [
-    {
-      parser: data => JSON.parse(data),
-      check: arg => (arg.indexOf('json') + 1),
-    },
-    {
-      parser: data => yaml.safeLoad(data),
-      check: arg => (arg.indexOf('yml') + 1),
-    },
-    {
-      parser: data => ini.parse(data),
-      check: arg => (arg.indexOf('ini') + 1),
-    },
-  ];
+  const parsersList = {
+    json: data => JSON.parse(data),
+    yml: data => yaml.safeLoad(data),
+    ini: data => ini.parse(data),
+  };
+  const typeOfFiles = path.extname(filePath1).slice(1);
   const dataOfFile1 = fs.readFileSync(filePath1, 'utf8');
   const dataOfFile2 = fs.readFileSync(filePath2, 'utf8');
-  const getParser = arg => _.find(parsersList, ({ check }) => check(arg));
-  const { parser } = getParser(filePath1);
-  const parsedDataOfFile1 = parser(dataOfFile1);
-  const parsedDataOfFile2 = parser(dataOfFile2);
+  const getParser = parsersList[typeOfFiles];
+  const parsedDataOfFile1 = getParser(dataOfFile1);
+  const parsedDataOfFile2 = getParser(dataOfFile2);
   const result = compareParsedData(parsedDataOfFile1, parsedDataOfFile2);
   return ['{', ...result, '}'].join('\n');
 };
