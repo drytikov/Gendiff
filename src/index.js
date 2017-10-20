3053ff5a4635b472586c9147fbf98f87fa7c8d23
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-20 09:35:01 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import ini from 'ini';
 import yaml from 'js-yaml';
-import compareParsedData from '../src/compareParsedData';
+import { iterAst, render } from '../src/compareParsedData';
 
 export default (filePath1, filePath2) => {
   const parsersList = {
@@ -13,9 +13,9 @@ export default (filePath1, filePath2) => {
   const typeOfFiles = path.extname(filePath1).slice(1);
   const dataOfFile1 = fs.readFileSync(filePath1, 'utf8');
   const dataOfFile2 = fs.readFileSync(filePath2, 'utf8');
-  const getParser = parsersList[typeOfFiles];
-  const parsedDataOfFile1 = getParser(dataOfFile1);
-  const parsedDataOfFile2 = getParser(dataOfFile2);
-  const result = compareParsedData(parsedDataOfFile1, parsedDataOfFile2);
-  return ['{', ...result, '}'].join('\n');
+  const parse = parsersList[typeOfFiles];
+  const parsedDataOfFile1 = parse(dataOfFile1);
+  const parsedDataOfFile2 = parse(dataOfFile2);
+  const result = render(iterAst(parsedDataOfFile1, parsedDataOfFile2));
+  return `${['{', ...result, '}'].join('\n')}\n`;
 };
