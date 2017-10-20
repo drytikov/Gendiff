5ce40770b3fd83f75d059cc326e2a7e141830fcc
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-20 09:33:52 +0300

    update

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,7 @@
+import fs from 'fs';
 import genDiff from '../src';
 
-const result = '{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}';
+const result = fs.readFileSync('__tests__/fixtures/result', 'utf8');
 
 test('genDiff(JSON)', () => {
   expect(genDiff('__tests__/fixtures/before.json', '__tests__/fixtures/after.json')).toBe(result);
