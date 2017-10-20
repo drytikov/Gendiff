1085a3bbe4de702331b46d1e957b38da4f91e6ac
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-20 16:47:04 +0300

    add test for plain output

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,16 +1,21 @@
 import fs from 'fs';
-import genDiff from '../src';
+import { genDiffToJson, genDiffToPlain } from '../src';
 
 const result = fs.readFileSync('__tests__/fixtures/result', 'utf8');
+const resultPlain = fs.readFileSync('__tests__/fixtures/resultPlain', 'utf8');
 
 test('genDiff(JSON)', () => {
-  expect(genDiff('__tests__/fixtures/before.json', '__tests__/fixtures/after.json')).toBe(result);
+  expect(genDiffToJson('__tests__/fixtures/before.json', '__tests__/fixtures/after.json')).toBe(result);
 });
 
 test('genDiff(YAML)', () => {
-  expect(genDiff('__tests__/fixtures/before.yml', '__tests__/fixtures/after.yml')).toBe(result);
+  expect(genDiffToJson('__tests__/fixtures/before.yml', '__tests__/fixtures/after.yml')).toBe(result);
 });
 
 test('genDiff(INI)', () => {
-  expect(genDiff('__tests__/fixtures/before.ini', '__tests__/fixtures/after.ini')).toBe(result);
+  expect(genDiffToJson('__tests__/fixtures/before.ini', '__tests__/fixtures/after.ini')).toBe(result);
+});
+
+test('genDiff(JSON) to plain', () => {
+  expect(genDiffToPlain('__tests__/fixtures/before.json', '__tests__/fixtures/after.json')).toBe(resultPlain);
 });
