980a56aa088f91df351cfd62ef0a26926544f0d7
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-20 22:55:01 +0300

    refactor

--- src/renderToPlain.js
+++ src/renderToPlain.js
@@ -1,15 +1,16 @@
 const renderToPlain = (ast, name) =>
   ast.filter(item => item.type !== 'equal').map((item) => {
     const itemName = (name === undefined) ? item.key : `${name}.${item.key}`;
+    const value = (item.value instanceof Object) ? 'complex value' : `value: ${item.value}`;
     switch (item.type) {
       case 'keysList':
         return renderToPlain(item.children, itemName).join('\n');
       case 'removed':
         return `Property '${itemName}' was removed`;
       case 'added':
-        return `Property '${itemName}' was added with value: ${item.value}`;
+        return `Property '${itemName}' was added with ${value}`;
       case 'updated':
-        return `Property '${itemName}' was updated. From '${item.oldValue}' to '${item.value}'`;
+        return `Property '${itemName}' was updated. From value: ${item.oldValue} to ${value}`;
       case 'removedWithChildren':
         return `Property '${itemName}' was removed`;
       case 'addedWithChildren':
