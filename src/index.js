b62606586981b80adc92f4a5220a88b297131699
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-20 16:48:12 +0300

    add plain output

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import fs from 'fs';
 import path from 'path';
 import ini from 'ini';
 import yaml from 'js-yaml';
-import { iterAst, render } from '../src/compareParsedData';
+import iterAst from '../src/compareParsedData';
+import renderToJson from '../src/renderToJson';
+import renderToPlain from '../src/renderToPlain';
 
-export default (filePath1, filePath2) => {
+const getParsedData = (filePath1, filePath2) => {
   const parsersList = {
     json: data => JSON.parse(data),
     yml: data => yaml.safeLoad(data),
@@ -16,6 +18,19 @@ export default (filePath1, filePath2) => {
   const parse = parsersList[typeOfFiles];
   const parsedDataOfFile1 = parse(dataOfFile1);
   const parsedDataOfFile2 = parse(dataOfFile2);
-  const result = render(iterAst(parsedDataOfFile1, parsedDataOfFile2));
+  return { parsedDataOfFile1, parsedDataOfFile2 };
+};
+
+export const genDiffToJson = (filePath1, filePath2) => {
+  const { parsedDataOfFile1, parsedDataOfFile2 } = getParsedData(filePath1, filePath2);
+  const result = renderToJson(iterAst(parsedDataOfFile1, parsedDataOfFile2));
+  console.log(`${['{', ...result, '}'].join('\n')}\n`);
   return `${['{', ...result, '}'].join('\n')}\n`;
+};
+
+export const genDiffToPlain = (filePath1, filePath2) => {
+  const { parsedDataOfFile1, parsedDataOfFile2 } = getParsedData(filePath1, filePath2);
+  const result = renderToPlain(iterAst(parsedDataOfFile1, parsedDataOfFile2));
+  console.log(`${result.join('\n')}\n`);
+  return `${result.join('\n')}\n`;
 };
