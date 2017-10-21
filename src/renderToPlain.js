b99a6758bda634bc5c4bca5a47e26b25fa63ee13
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-21 14:54:50 +0300

    refactor

--- src/renderToPlain.js
+++ src/renderToPlain.js
@@ -1,25 +1,21 @@
-const renderToPlain = (ast, name) =>
+const result = (ast, name) =>
   ast.filter(item => item.type !== 'equal').map((item) => {
     const itemName = (name === undefined) ? item.key : `${name}.${item.key}`;
-    const value = (item.value instanceof Object) ? 'complex value' : `value: ${item.value}`;
+    const value = (item.curValue instanceof Object) ? 'complex value' : `value: ${item.curValue}`;
     switch (item.type) {
-      case 'keysList':
-        return renderToPlain(item.children, itemName).join('\n');
+      case 'nested':
+        return result(item.children, itemName).join('\n');
       case 'removed':
         return `Property '${itemName}' was removed`;
       case 'added':
         return `Property '${itemName}' was added with ${value}`;
       case 'updated':
         return `Property '${itemName}' was updated. From value: ${item.oldValue} to ${value}`;
-      case 'removedWithChildren':
-        return `Property '${itemName}' was removed`;
-      case 'addedWithChildren':
-        return `Property '${itemName}' was added with complex value`;
-      case 'updatedWithChildren':
-        return `Property '${itemName}' was updated. From '${item.oldValue}' to complex value`;
       default:
         return item;
     }
   });
+
+const renderToPlain = ast => `${result(ast).join('\n')}\n`;
 
 export default renderToPlain;
