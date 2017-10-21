015b58fe87d1709f96ee18c4bb95fc7e6dae219a
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-21 14:54:30 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -6,11 +6,15 @@ import iterAst from '../src/compareParsedData';
 import renderToJson from '../src/renderToJson';
 import renderToPlain from '../src/renderToPlain';
 
-const getParsedData = (filePath1, filePath2) => {
+export default (filePath1, filePath2, outputFormat) => {
   const parsersList = {
     json: data => JSON.parse(data),
     yml: data => yaml.safeLoad(data),
     ini: data => ini.parse(data),
+  };
+  const outputFormatsList = {
+    json: ast => renderToJson(ast),
+    plain: ast => renderToPlain(ast),
   };
   const typeOfFiles = path.extname(filePath1).slice(1);
   const dataOfFile1 = fs.readFileSync(filePath1, 'utf8');
@@ -18,19 +22,8 @@ const getParsedData = (filePath1, filePath2) => {
   const parse = parsersList[typeOfFiles];
   const parsedDataOfFile1 = parse(dataOfFile1);
   const parsedDataOfFile2 = parse(dataOfFile2);
-  return { parsedDataOfFile1, parsedDataOfFile2 };
-};
-
-export const genDiffToJson = (filePath1, filePath2) => {
-  const { parsedDataOfFile1, parsedDataOfFile2 } = getParsedData(filePath1, filePath2);
-  const result = renderToJson(iterAst(parsedDataOfFile1, parsedDataOfFile2));
-  console.log(`${['{', ...result, '}'].join('\n')}\n`);
-  return `${['{', ...result, '}'].join('\n')}\n`;
-};
-
-export const genDiffToPlain = (filePath1, filePath2) => {
-  const { parsedDataOfFile1, parsedDataOfFile2 } = getParsedData(filePath1, filePath2);
-  const result = renderToPlain(iterAst(parsedDataOfFile1, parsedDataOfFile2));
-  console.log(`${result.join('\n')}\n`);
-  return `${result.join('\n')}\n`;
+  const render = outputFormatsList[outputFormat];
+  const result = render(iterAst(parsedDataOfFile1, parsedDataOfFile2));
+  console.log(result);
+  return result;
 };
