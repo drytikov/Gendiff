98126f20f1545b464e4c00765e5d4ea8f8b0dcea
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-21 14:53:42 +0300

    add action for different  output formats

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -7,6 +7,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig>, <secondConfig>')
   .option('-f, --format [type]', 'Output format')
-  .action((firstConfig, secondConfig) => genDiff(firstConfig, secondConfig));
+  .action((firstConfig, secondConfig, options) => {
+    const outputFormat = options.format || 'json';
+    return genDiff(firstConfig, secondConfig, outputFormat);
+  });
 
 program.parse(process.argv);
