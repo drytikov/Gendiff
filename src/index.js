4d3affe5e07a553139c63dec6991ced010139558
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-22 18:15:09 +0300

    delete console.log

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import getRender from '../src/rendersToOutputFormats';
 
 export default (filePath1, filePath2, outputFormat) => {
   const parsersList = {
-    json: data => JSON.parse(data),
-    yml: data => yaml.safeLoad(data),
-    ini: data => ini.parse(data),
+    json: JSON.parse,
+    yml: yaml.safeLoad,
+    ini: ini.parse,
   };
   const typeOfFiles = path.extname(filePath1).slice(1);
   const dataOfFile1 = fs.readFileSync(filePath1, 'utf8');
@@ -18,7 +18,5 @@ export default (filePath1, filePath2, outputFormat) => {
   const parsedDataOfFile1 = parse(dataOfFile1);
   const parsedDataOfFile2 = parse(dataOfFile2);
   const render = getRender(outputFormat);
-  const result = render(iterAst(parsedDataOfFile1, parsedDataOfFile2));
-  console.log(result);
-  return result;
+  return render(iterAst(parsedDataOfFile1, parsedDataOfFile2));
 };
