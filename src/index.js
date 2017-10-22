49a66b9d0438a483f4f480e5168efa67e420b828
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-22 14:57:04 +0300

    refacor

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import path from 'path';
 import ini from 'ini';
 import yaml from 'js-yaml';
 import iterAst from '../src/compareParsedData';
-import renderToSructuredText from '../src/renderToStructuredText';
-import renderToPlain from '../src/renderToPlain';
-import renderToJson from '../src/renderToJson';
+import getRender from '../src/rendersToOutputFormats';
 
 export default (filePath1, filePath2, outputFormat) => {
   const parsersList = {
@@ -13,18 +11,13 @@ export default (filePath1, filePath2, outputFormat) => {
     yml: data => yaml.safeLoad(data),
     ini: data => ini.parse(data),
   };
-  const outputFormatsList = {
-    sructuredText: ast => renderToSructuredText(ast),
-    plain: ast => renderToPlain(ast),
-    json: ast => renderToJson(ast),
-  };
   const typeOfFiles = path.extname(filePath1).slice(1);
   const dataOfFile1 = fs.readFileSync(filePath1, 'utf8');
   const dataOfFile2 = fs.readFileSync(filePath2, 'utf8');
   const parse = parsersList[typeOfFiles];
   const parsedDataOfFile1 = parse(dataOfFile1);
   const parsedDataOfFile2 = parse(dataOfFile2);
-  const render = outputFormatsList[outputFormat];
+  const render = getRender(outputFormat);
   const result = render(iterAst(parsedDataOfFile1, parsedDataOfFile2));
   console.log(result);
   return result;
