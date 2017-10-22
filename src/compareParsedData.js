bf5f3d7c4b6c1d1231adccf357d2e2e28a95b45f
Rytikov Dmitrii <dmitry@rytikovi.com>
2017-10-22 15:09:35 +0300

    refactor

--- src/compareParsedData.js
+++ src/compareParsedData.js
@@ -2,24 +2,24 @@ import _ from 'lodash';
 
 const iterAst = (parsedData1, parsedData2) => {
   const uniqueItems = _.union(Object.keys(parsedData1), Object.keys(parsedData2));
-  return uniqueItems.reduce((acc, key) => {
+  return uniqueItems.map((key) => {
     if (parsedData1[key] instanceof Object && parsedData2[key] instanceof Object) {
-      return [...acc, {
+      return {
         type: 'nested', key, value: '', children: iterAst(parsedData1[key], parsedData2[key]),
-      }];
+      };
     }
     if (parsedData1[key] && parsedData2[key] === undefined) {
-      return [...acc, { type: 'removed', key, curValue: parsedData1[key] }];
+      return { type: 'removed', key, curValue: parsedData1[key] };
     }
     if (parsedData2[key] && parsedData1[key] === undefined) {
-      return [...acc, { type: 'added', key, curValue: parsedData2[key] }];
+      return { type: 'added', key, curValue: parsedData2[key] };
     }
     if (parsedData2[key] !== parsedData1[key]) {
-      return [...acc, {
+      return {
         type: 'updated', key, curValue: parsedData2[key], oldValue: parsedData1[key],
-      }];
+      };
     }
-    return [...acc, { type: 'equal', key, curValue: parsedData2[key] }];
+    return { type: 'equal', key, curValue: parsedData2[key] };
   }, []);
 };
 
